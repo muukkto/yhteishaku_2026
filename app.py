d1e31d9958529fe61d0c8f450026b07bec7b9213
Topias Muukkonen <topias.muukkonen@gmail.com>
2026-04-22 01:28:15 +0300

    More plots

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import pandas as pd
 import plotly.express as px
 from shinywidgets import render_plotly
 
+exams = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
 
 @reactive.file_reader(filepath = "./analysis/study_programmes.json")
 def study_programme_read(filepath = "./analysis/study_programmes.json"):
@@ -53,6 +54,54 @@ def exams_co_occurrence():
     return co_occurrence
 
 @reactive.calc
+def exams_by_participant():
+    study_programme_data = study_programme_dataset()
+    application_data = applications_dataset()
+    exams = {}
+
+    for application in application_data.values():
+        participant_exams = set()
+        for i in range(1, 7):
+            study_programme = application["study_programmes"][str(i)]
+            if study_programme is not None and study_programme in study_programme_data:
+                participant_exams.add(study_programme_data[study_programme]['exam'])
+
+        exams[application["id"]] = participant_exams
+
+    return exams
+
+@reactive.calc
+def participant_exam_count_distribution():
+    exams_by_participant_data = exams_by_participant()
+    distribution = {exam: {} for exam in exams}
+
+    for exam in exams:
+        for participant_exams in exams_by_participant_data.values():
+            if exam in participant_exams:
+                exam_count = len(participant_exams)
+                distribution[exam][exam_count] = distribution[exam].get(exam_count, 0) + 1
+
+    return distribution
+
+@reactive.calc
+def participant_exam_study_programme_distribution():
+    exams_by_participant_data = exams_by_participant()
+    application_data = applications_dataset()
+    study_programme_data = study_programme_dataset()
+    distribution = {study_programme: {} for study_programme in study_programme_data}
+
+    for study_programme in study_programme_data:
+        for application in application_data.values():
+            for i in range(1, 7):
+                if application["study_programmes"][str(i)] == study_programme:
+                    participant_exams = exams_by_participant_data.get(application["id"], set())
+                    exam_count = len(participant_exams)
+                    distribution[study_programme][exam_count] = distribution[study_programme].get(exam_count, 0) + 1
+                    break
+
+    return distribution
+
+@reactive.calc
 def exam_co_occurrence_distribution():
     co_occurrence = exams_co_occurrence()
     exam = selected_exam()
@@ -80,6 +129,41 @@ def wish_distribution():
                 if study_programme_data[study_programme]['exam'] == exam:
                     distribution[i] = distribution.get(i, 0) + 1
                     break
+
+    return distribution
+
+@reactive.calc
+def wish_count_distribution():
+    study_programme_data = study_programme_dataset()
+    application_data = applications_dataset()
+
+    distribution = {"known": {}, "unknown": {}, "all": {}}
+
+    j = 0
+
+    for application in application_data.values():
+        wish_count_known_study_programme = 0
+        wish_count_unknown_study_programme = 0
+
+        for i in range(1, 7):
+            study_programme = application["study_programmes"][str(i)]
+            if study_programme is not None:
+                if study_programme in study_programme_data:
+                    wish_count_known_study_programme += 1
+                else:
+                    wish_count_unknown_study_programme += 1
+
+        wish_count_all_study_programme = wish_count_known_study_programme + wish_count_unknown_study_programme
+
+        if wish_count_known_study_programme == 0:
+            print(f"Application {j} has no known study programmes.")
+            print(application)
+
+        j += 1
+
+        distribution["known"][wish_count_known_study_programme] = distribution["known"].get(wish_count_known_study_programme, 0) + 1
+        distribution["unknown"][wish_count_unknown_study_programme] = distribution["unknown"].get(wish_count_unknown_study_programme, 0) + 1
+        distribution["all"][wish_count_all_study_programme] = distribution["all"].get(wish_count_all_study_programme, 0) + 1
 
     return distribution
 
@@ -151,6 +235,83 @@ def get_selectize_choices_uni():
     choices = {uni: uni for uni in universities}
     return choices
 
+with ui.nav_panel("Yleiskatsaus"):
+    @render.plot
+    def co_occurrence_heatmap():
+        co_occurrence = exams_co_occurrence()
+        exams = sorted(set(exam for exam_pair in co_occurrence.keys() for exam in exam_pair))
+        exam_index = {exam: idx for idx, exam in enumerate(exams)}
+
+        matrix = [[0] * len(exams) for _ in range(len(exams))]
+
+        for (exam1, exam2), count in co_occurrence.items():
+            i, j = exam_index[exam1], exam_index[exam2]
+            matrix[i][j] = count
+            matrix[j][i] = count
+
+        fig, ax = plt.subplots()
+
+        cax = ax.matshow(matrix, cmap='Blues')
+        fig.colorbar(cax)
+
+        ax.set_xticks(range(len(exams)))
+        ax.set_yticks(range(len(exams)))
+        ax.set_xticklabels(exams, rotation=90)
+        ax.set_yticklabels(exams)
+
+        ax.set_title("Valintakokeiden yhteishakujen lämpökartta")
+
+        return fig
+    
+    @render.plot
+    def participant_exam_count_histogram_overview():
+        exams_by_participant_data = exams_by_participant()
+        distribution = {}
+
+        for participant_exams in exams_by_participant_data.values():
+            exam_count = len(participant_exams)
+            distribution[exam_count] = distribution.get(exam_count, 0) + 1
+
+        keys = sorted(distribution.keys())
+        values = [distribution[k] for k in keys]
+
+        fig, ax = plt.subplots()
+        ax_container = ax.bar(keys, values)
+        ax.bar_label(ax_container, label_type='center')
+
+        ax.set_xlabel("Valintakokeiden määrä")
+        ax.set_ylabel("Hakijoita")
+        ax.set_title("Hakijoiden valintakokeiden määrä")
+
+        return fig
+    
+    ui.input_switch("exam_switch", "Tarkastele vain yliopistojen valintakokeita käyttäviä hakutoiveita", False) 
+
+    @render.plot
+    def wish_histogram_overview():
+        distribution = wish_count_distribution()
+
+        if input.exam_switch():
+            keys = sorted(distribution["known"].keys())
+            values = [distribution["known"][k] for k in keys]
+            title = 'Hakutoiveiden määrä (vain yliopistojen valintakokeita käyttävät hakukohteet)'
+        else:
+            keys = sorted(distribution["all"].keys())
+            values = [distribution["all"][k] for k in keys]
+            title = 'Hakutoiveden määrä (kaikki hakukohteet)'
+
+        fig, ax = plt.subplots()
+        ax_container = ax.bar(keys, values)
+
+        ax.set_xlabel("Hakutoiveiden määrä")
+        ax.set_ylabel("Hakijoita")
+        ax.set_title(title)
+        ax.bar_label(ax_container, label_type='center')
+
+        return fig
+        
+
+
 with ui.nav_panel("Koekohtainen tarkastelu"):
     ui.input_select(  
         "exam",  
@@ -182,6 +343,24 @@ with ui.nav_panel("Koekohtainen tarkastelu"):
         ax.set_xlabel("Valintakoe")
         ax.set_ylabel("Hakijoita")
         ax.set_title(f"Valintakokeen {selected_exam()} hakijoiden muut valintakokeet")
+
+        return fig
+    
+    @render.plot
+    def participant_exam_count_histogram():
+        distribution = participant_exam_count_distribution()
+        exam = selected_exam()
+
+        keys = sorted(distribution[exam].keys())
+        values = [distribution[exam][k] for k in keys]
+
+        fig, ax = plt.subplots()
+        ax_container = ax.bar(keys, values)
+        ax.bar_label(ax_container, label_type='center')
+
+        ax.set_xlabel("Valintakokeiden määrä")
+        ax.set_ylabel("Hakijoita")
+        ax.set_title(f"Valintakokeen {exam} hakijoiden valintakokeiden määrä")
 
         return fig
     
@@ -265,4 +444,23 @@ with ui.nav_panel("Hakukohteet"):
                          path=['label'], 
                          values='count',
                          title=f"Hakukohteen {sp_name} ristihakukohteet")
+        return fig
+
+    @render.plot
+    def participant_exam_count_histogram_study_programme():
+        distribution = participant_exam_study_programme_distribution()
+        study_programme = selected_study_programme()
+
+        keys = sorted(distribution[study_programme].keys())
+        values = [distribution[study_programme][k] for k in keys]
+
+        fig, ax = plt.subplots()
+        ax_container = ax.bar(keys, values)
+        ax.bar_label(ax_container, label_type='center')
+
+        ax.set_xlabel("Valintakokeiden määrä")
+        ax.set_ylabel("Hakijoita")
+        sp_name = study_programme_dataset().get(study_programme, {}).get("name", "tuntematon")
+        ax.set_title(f"Hakukohteen {sp_name}\nhakijoiden valintakokeiden määrä")
+
         return fig
